54ed82e124c7fa309b69e4bdc8aaf296ef0b140b
Erin Millard <contact@erin.id.au>
2024-05-11 14:22:48 +1000

    Remove ugly whitespace from comment

--- latlon.go
+++ latlon.go
@@ -11,8 +11,7 @@ import (
 //
 // lat and lon are given in radians.
 //
-// See:
-// https://github.com/FFI-no/n-vector/blob/82d749a67cc9f332f48c51aa969cdc277b4199f2/nvector/lat_long2n_E.m
+// See: https://github.com/FFI-no/n-vector/blob/82d749a67cc9f332f48c51aa969cdc277b4199f2/nvector/lat_long2n_E.m
 func FromLatLon(lat, lon float64, opts ...Option) mat.Vector {
 	o := options.New(opts)
 
