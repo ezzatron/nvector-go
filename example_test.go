ec03331afdeda784b011afe3a2c1336309698706
Erin Millard <contact@erin.id.au>
2024-05-14 22:03:15 +1000

    Add example 9 as a testable example

--- example_test.go
+++ example_test.go
@@ -368,3 +368,47 @@ func Example_n08() {
 	// Output:
 	// Destination: lat, long = 79.99154867, -90.01769837 deg
 }
+
+// Example 9: Intersection of two paths
+//
+// Given path A going through A(1) and A(2), and path B going through B(1) and
+// B(2). Find the intersection of the two paths.
+//
+// See: https://www.ffi.no/en/research/n-vector/#example_9
+func Example_n09() {
+	// Two paths A and B are given by two pairs of positions:
+	// Enter elements directly:
+	// a1 := r3.Unit(r3.Vec{X: 0, Y: 0, Z: 1})
+	// a2 := r3.Unit(r3.Vec{X: -1, Y: 0, Z: 1})
+	// b1 := r3.Unit(r3.Vec{X: -2, Y: -2, Z: 4})
+	// b2 := r3.Unit(r3.Vec{X: -2, Y: 2, Z: 2})
+
+	// or input as lat/long in deg:
+	a1 := FromLatLon(Rad(50), Rad(180))
+	a2 := FromLatLon(Rad(90), Rad(180))
+	b1 := FromLatLon(Rad(60), Rad(160))
+	b2 := FromLatLon(Rad(80), Rad(-140))
+
+	// SOLUTION:
+
+	// Find the intersection between the two paths, n_EC_E:
+	cTmp := r3.Cross(r3.Cross(a1, a2), r3.Cross(b1, b2))
+
+	// n_EC_E_tmp is one of two solutions, the other is -n_EC_E_tmp. Select the
+	// one that is closest to n_EA1_E, by selecting sign from the dot product
+	// between n_EC_E_tmp and n_EA1_E:
+	c := r3.Scale(math.Copysign(1, r3.Dot(cTmp, a1)), cTmp)
+
+	// When displaying the resulting position for humans, it is more convenient
+	// to see lat, long:
+	lat, lon := ToLatLon(c)
+
+	fmt.Printf(
+		"Intersection: lat, long = %.8f, %.8f deg\n",
+		Deg(lat),
+		Deg(lon),
+	)
+
+	// Output:
+	// Intersection: lat, long = 74.16344802, 180.00000000 deg
+}
