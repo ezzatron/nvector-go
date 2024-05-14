95df2069eb3bb616b7d3ad9a17681f1e2e16db25
Erin Millard <contact@erin.id.au>
2024-05-14 21:06:06 +1000

    Add example 5 as a testable example

--- example_test.go
+++ example_test.go
@@ -119,7 +119,12 @@ func Example_n02() {
 	// Here we also assume that the user wants the output to be height (= -depth):
 	cHeight := -cDepth
 
-	fmt.Printf("Pos C: lat, long = %.8f, %.8f deg, height = %.8f m\n", Deg(lat), Deg(lon), cHeight)
+	fmt.Printf(
+		"Pos C: lat, long = %.8f, %.8f deg, height = %.8f m\n",
+		Deg(lat),
+		Deg(lon),
+		cHeight,
+	)
 
 	// Output:
 	// Pos C: lat, long = 53.32637826, 63.46812344 deg, height = 406.00719607 m
@@ -150,7 +155,12 @@ func Example_n03() {
 	// const h_EB = -z_EB;
 	hb := -db
 
-	fmt.Printf("Pos B: lat, long = %.8f, %.8f deg, height = %.8f m\n", Deg(lat), Deg(lon), hb)
+	fmt.Printf(
+		"Pos B: lat, long = %.8f, %.8f deg, height = %.8f m\n",
+		Deg(lat),
+		Deg(lon),
+		hb,
+	)
 
 	// Output:
 	// Pos B: lat, long = 5.68507573, -45.40066326 deg, height = 95772.10761822 m
@@ -182,4 +192,42 @@ func Example_n04() {
 
 	// Output:
 	// p_EB_E = [6373290.27721828, 222560.20067474, 110568.82718179] m
+}
+
+// Example 5: Surface distance
+//
+// Given position A and B. Find the surface distance (i.e. great circle
+// distance) and the Euclidean distance.
+//
+// See: https://www.ffi.no/en/research/n-vector/#example_5
+func Example_n05() {
+	// Position A and B are given as n_EA_E and n_EB_E:
+	// Enter elements directly:
+	// a := r3.Unit(r3.Vec{X: 1, Y: 0, Z: -2})
+	// b := r3.Unit(r3.Vec{X: -1, Y: -2, Z: 0})
+
+	// or input as lat/long in deg:
+	a := FromLatLon(Rad(88), Rad(0))
+	b := FromLatLon(Rad(89), Rad(-170))
+
+	// m, mean Earth radius
+	re := 6371e3
+
+	// SOLUTION:
+
+	// The great circle distance is given by equation (16) in Gade (2010):
+	// Well conditioned for all angles:
+	sab := math.Atan2(r3.Norm(r3.Cross(a, b)), r3.Dot(a, b)) * re
+
+	// The Euclidean distance is given by:
+	dab := r3.Norm(r3.Sub(b, a)) * re
+
+	fmt.Printf(
+		"Great circle distance = %.8f km, Euclidean distance = %.8f km\n",
+		sab/1000,
+		dab/1000,
+	)
+
+	// Output:
+	// Great circle distance = 332.45644411 km, Euclidean distance = 332.41872486 km
 }
